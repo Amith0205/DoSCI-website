12f4c1343bb89cd13d8650086c7c36d6b85bebbb
Nitin Kumar <snitin315@gmail.com>
2020-10-17 07:00:20 +0530

    add about tab

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
   devise_for :users
   ActiveAdmin.routes(self)
   get '/index'   => "home#index"
-  get '/about_institute' => "home#about_institute"
+  get '/about_iet' => "home#about_iet"
   get '/downloads_page' => "home#downloads_page"
   get '/call_for_special_sessions' => 'home#call_for_special_sessions'
   get '/special_session' => 'home#call_for_special_sessions'
   get '/call_for_poster_paper' => 'home#call_for_poster_paper'
   get '/new_special_session' => 'home#special_session'
   get '/download' => 'home#download'
-  get '/about_irdindia' => "home#about_irdindia"
+  get '/about_aktu' => "home#about_aktu"
   get '/about_universal_innovators' => "home#about_universal_innovators"
   get '/call_for_papers' => "home#call_for_papers"
   get '/registration' => "home#registration"
